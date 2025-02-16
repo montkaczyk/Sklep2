b51983b6fb9ab7bec89c86079b13bf1e21c56364
montkaczyk <tkaczykmonika@interia.pl>
2025-02-16 16:53:38 +0100

    Przeniesienie plików na główny poziom repozytorium

--- Sklep-master/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace sklep.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
